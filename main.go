5a59b6b25865ebe98bd11fb3b3fee17aa9e30956
satit13 <satit@nopadol.com>
2018-03-06 15:23:02 +0700

    meta coding

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/lib/pq"
 	//"github.com/mrtomyum/paybox_cloud/vending"
 	"encoding/json"
+	"strconv"
 )
 
 const (
@@ -21,20 +22,6 @@ const (
 	dbName = "paybox"
 )
 
-func main() {
-	log.Println("Paybox Clients Service")
-	conn := fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%s sslmode=%s",
-		dbName, dbUser, dbPass, dbHost, dbPort, sslMode)
-
-	fmt.Println(conn)
-	//init db
-	db, err := sql.Open("postgres", conn)
-	must(err)
-	err = db.Ping()
-	must(err)
-	GetClient(db, 0)
-	defer db.Close()
-}
 
 func must(err error) {
 	if err != nil {
@@ -61,7 +48,7 @@ type Content struct {
 type Client  struct {
 	Shop_title               string `json:"shop_title"`
 	Shop_website             string `json:"shop_website"`
-	LogoImage_width          string `json:"logo_image_width"`
+	LogoImage_width          int `json:"logo_image_width"`
 	Api_server_url           string `json:"api_server_url"`
 	Cloud_server_url         string `json:"cloud_server_url"`
 	Data_url                 string `json:"data_url"`
@@ -71,48 +58,74 @@ type Client  struct {
 }
 
 type Cash  struct {
-	Max_change_cash int
-	Max_change_coin int
-	Min_change      int
+	Max_change_cash int `json:"max_change_cash"`
+	Max_change_coin int `json:"max_change_coin"`
+	Min_change      int `json:"min_change"`
 }
 
 type CashAcceptedValue struct {
-	B20   int
-	B50   int
-	B100  int
-	B500  int
-	B1000 int
+	B20   int `json:"b_20"`
+	B50   int `json:"b_50"`
+	B100  int `json:"b_100"`
+	B500  int `json:"b_500"`
+	B1000 int `json:"b_1000"`
 }
 
 type ChangeRefillValue struct {
-	V1    int
-	V2    int
-	V5    int
-	V10   int
-	V20   int
-	V50   int
-	V100  int
-	V500  int
-	V1000 int
+	V1    int `json:"v1"`
+	V2    int `json:"v2"`
+	V5    int `json:"v5"`
+	V10   int `json:"v10"`
+	V20   int `json:"v20"`
+	V50   int `json:"v50"`
+	V100  int `json:"v100"`
+	V500  int `json:"v500"`
+	V1000 int `json:"v1000"`
 }
 
 type ClientTable struct {
 	Id   int64
-	Meta sql.NullString
+	Meta string
 }
 
-func GetClient(db *sql.DB, id int64) (error) {
-	_c := ClientTable{}
+
+func main() {
+	log.Println("Meta Testt ")
+	conn := fmt.Sprintf("dbname=%s user=%s password=%s host=%s port=%s sslmode=%s",
+		dbName, dbUser, dbPass, dbHost, dbPort, sslMode)
+
+	fmt.Println(conn)
+	//init db
+	db, err := sql.Open("postgres", conn)
+	must(err)
+	err = db.Ping()
+	must(err)
+
+	err = GetClient(db,1)
+	must(err)
+	defer db.Close()
+}
+
+func GetClient(db *sql.DB,pid int) (error) {
+	//_c := ClientTable{}
 	m := Meta{}
-	lccommand := "select id,meta from clients where id = ?"
+	lccommand := "select id,meta from clients where id =  "+strconv.Itoa(pid)
+	//lccommand := "select id,meta from clients where id = ? "
+
 	fmt.Println(lccommand)
-	rs := db.QueryRow(lccommand, id)
-	rs.Scan(&_c)
+	fmt.Println("pid = ",pid)
+	rs := db.QueryRow(lccommand)
+	//rs := db.QueryRow(lccommand,pid)
 
-	fmt.Println(_c)
-	stringMeta := _c.Meta.String
+	var _meta string
+	var _id int
+	rs.Scan(&_id,&_meta)
+	fmt.Printf("client_id = %v\n ", _id)
+	//stringMeta := _c.Meta
 
-	_ = json.Unmarshal([]byte(stringMeta), &m)
-	fmt.Println(m)
+	_ = json.Unmarshal([]byte(_meta), &m)
+
+	fmt.Println("meta object " , m)
+	fmt.Println(m.ClientConfig.Logo)
 	return nil
 }
